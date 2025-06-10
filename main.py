e258d560a4a637611a16b820eb85a948c2e027ce
akashsingh <singhakashkumar.2020@gmail.com>
2025-06-10 22:19:10 +0530

    pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title : str
+    content : str
 
 @app.get("/")
 def root():
@@ -11,5 +17,6 @@ def get_posts():
     return {"data": "these are all my posts"}
 
 @app.post("/createpost")
-def create_post():
+def create_post(payload : Post):
+    print(payload)
     return {"message": "post successfully created."}
